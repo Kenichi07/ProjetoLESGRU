d0db3b8919fa529ba7b81ba4ff2bef096289ee10
Vinicius Huang <viniciushuangbr@gmail.com>
2025-02-23 16:43:06 -0300

    first commit

---
+++ ProjetoLESGRU/src/main/java/com/example/demo/ProjetoLesgruApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoLesgruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoLesgruApplication.class, args);
+	}
+
+}
